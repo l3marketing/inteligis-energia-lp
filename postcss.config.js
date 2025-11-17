0a58162d8d2b38ce06f143fb5cb18b652402f386
L3 Marketing <contato@l3marketing.com.br>
2025-11-17 19:06:20 -0300

    upload 9

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
+import tailwindcss from "tailwindcss/lib/index.js";
+import autoprefixer from "autoprefixer";
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss(), autoprefixer()],
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  const plugins = [react()];
   return {
     base: env.VITE_BASE_PATH || "/",
     server: {
       host: "::",
       port: 8080,
     },
-    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+    plugins,
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
