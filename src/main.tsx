cb03a9572c9c07533470a50842b7ab0f2bb50f79
leolameirao <leonardolameiro@yahoo.com.br>
2025-11-17 13:57:02 -0300

    upload 8

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+if (redirect) {
+  history.replaceState(null, "", redirect);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
